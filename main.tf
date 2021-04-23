a3ec83debc2d64069307b0f266b3acdf6e348d68
Paul-Emmanuel Raoul <skyper@skyplabs.net>
2021-04-23 17:32:38 +0100

    Move 'ovh' provider into 'providers.tf'

--- main.tf
+++ main.tf
@@ -1,7 +1,1 @@
-# https://registry.terraform.io/providers/ovh/ovh.
-provider "ovh" {
-  endpoint           = var.ovh_endpoint
-  application_key    = var.ovh_application_key
-  application_secret = var.ovh_application_secret
-  consumer_key       = var.ovh_consumer_key
-}
+
---
+++ providers.tf
@@ -0,0 +1,7 @@
+# https://registry.terraform.io/providers/ovh/ovh.
+provider "ovh" {
+  endpoint           = var.ovh_endpoint
+  application_key    = var.ovh_application_key
+  application_secret = var.ovh_application_secret
+  consumer_key       = var.ovh_consumer_key
+}
